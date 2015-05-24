35548918947521e18e112daa508ddb8ee5d678ff
Andrea Marini <andrea.marini@cern.ch>
2015-05-24 12:10:33 +0200

    adding LinkDef file

---
+++ Nero/interface/BareLinkDef.hpp
@@ -0,0 +1,14 @@
+#ifdef __CINT__
+
+#pragma link C++ class BareCollection+;
+#pragma link C++ class BareEvent+;
+#pragma link C++ class BareFatJets+;
+#pragma link C++ class BareJets+;
+#pragma link C++ class BareLeptons+;
+#pragma link C++ class BareMet+;
+#pragma link C++ class BareMonteCarlo+;
+#pragma link C++ class BarePhotons+;
+#pragma link C++ class BareTaus+;
+#pragma link C++ class BareVertex+;
+
+#endif
