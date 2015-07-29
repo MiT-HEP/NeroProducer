5e28106bb52b17908f5e40a472d228e47d326c4d
Yutaro Iiyama <yiiyama@mit.edu>
2015-07-29 05:10:21 -0400

    filling new met variables

--- Bambu/source/MetFiller.cc
+++ Bambu/source/MetFiller.cc
@@ -3,6 +3,7 @@
 #include "MitAna/DataTree/interface/MetCol.h"
 #include "MitAna/DataTree/interface/GenMetCol.h"
 #include "MitAna/DataTree/interface/ParticleCol.h"
+#include "MitAna/DataTree/interface/PFCandidateCol.h"
 #include "MitAna/DataTree/interface/EventHeader.h"
 #include "MitAna/DataTree/interface/Names.h"
 
@@ -25,6 +26,34 @@ mithep::nero::MetFiller::fill()
   for (unsigned iM(0); iM != muons->GetEntries(); ++iM)
     metNoMu += muons->At(iM)->Mom();
   out_.metNoMu = metNoMu.Pt();
+
+  mithep::FourVectorM pChargedHadron;
+  mithep::FourVectorM pNeutralHadron;
+  mithep::FourVectorM pNeutralEM;
+  auto* pfCandidates = getSource<mithep::PFCandidateCol>(pfCandidatesName_);
+  for (unsigned iP(0); iP != pfCandidates->GetEntries(); ++iP) {
+    auto& cand(*pfCandidates->At(iP));
+
+    switch (cand.PFType()) {
+    case mithep::PFCandidate::eHadron:
+      pChargedHadron += cand.Mom();
+      break;
+    case mithep::PFCandidate::eNeutralHadron:
+    case mithep::PFCandidate::eHadronHF:
+      pNeutralHadron += cand.Mom();
+      break;
+    case mithep::PFCandidate::eGamma:
+    case mithep::PFCandidate::eEGammaHF:
+      pNeutralEM += cand.Mom();
+      break;
+    default:
+      break;
+    }
+  }
+
+  out_.metChargedHadron = pChargedHadron.Pt();
+  out_.metNeutralHadron = pNeutralHadron.Pt();
+  out_.metNeutralEM = pNeutralEM.Pt();
 
   if (getSource<mithep::EventHeader>(Names::gkEvtHeaderBrn)->IsMC()) {
     auto* genMetCol = getSource<mithep::MetCol>(genMetName_);
