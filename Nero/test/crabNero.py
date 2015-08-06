d155446e366b997cbb18a554e0e86d20cd574998
Andrea Marini <andrea.marini@cern.ch>
2015-08-06 11:51:33 +0200

    multicrab

--- Nero/test/crabNero.py
+++ Nero/test/crabNero.py
@@ -23,9 +23,42 @@ config.Data.ignoreLocality = True
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1
 config.Data.totalUnits = -1
-config.Data.outLFNDirBase = '/store/user/%s/Nero/v1.0/' % (getUsernameFromSiteDB())
-config.Data.publication = True
-config.Data.publishDataName ='RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9_RECO'
+config.Data.outLFNDirBase = '/store/user/%s/Nero/v1.0.3/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+config.Data.publishDataName =''
 
 config.Site.storageSite = 'T2_CH_CERN'
+
+
+if __name__ == '__main__':
+
+    from CRABAPI.RawCommand import crabCommand
+    from CRABClient.ClientExceptions import ClientException
+    from httplib import HTTPException
+
+    # We want to put all the CRAB project directories from the tasks we submit here into one common directory.
+    # That's why we need to set this parameter (here or above in the configuration file, it does not matter, we will not overwrite it).
+    config.General.workArea = 'crab_projects'
+
+    def submit(config):
+        try:
+            crabCommand('submit', config = config)
+        except HTTPException as hte:
+            print "Failed submitting task: %s" % (hte.headers)
+        except ClientException as cle:
+            print "Failed submitting task: %s" % (cle)
+
+    #############################################################################################
+    ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
+    #############################################################################################
+
+    config.General.requestName = 'WZ-25ns'
+    config.Data.inputDataset = '/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+    config.Data.unitsPerJob = 10
+    submit(config)
+
+    config.General.requestName = 'ZZ-25ns'
+    config.Data.inputDataset = '/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/MINIAODSIM'
+    config.Data.unitsPerJob = 10
+    submit(config)
 
