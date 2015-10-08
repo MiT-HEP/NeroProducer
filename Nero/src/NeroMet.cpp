3c110ca5610256d56c49cbee67047ba968ca052b
Andrea Marini <andrea.marini@cern.ch>
2015-10-08 14:06:37 +0200

    indentation

--- Nero/src/NeroMet.cpp
+++ Nero/src/NeroMet.cpp
@@ -2,8 +2,8 @@
 #include "NeroProducer/Nero/interface/Nero.hpp"
 
 NeroMet::NeroMet() : 
-        NeroCollection(),
-        BareMet() 
+    NeroCollection(),
+    BareMet() 
 {
     pf = NULL;
 }
@@ -32,7 +32,7 @@ int NeroMet::analyze(const edm::Event& iEvent){
     caloMet_Pt = met.caloMETPt();
     caloMet_Phi = met.caloMETPhi();
     caloMet_SumEt = met.caloMETSumEt();
-    
+
 
     // FILL
     new ( (*p4)[p4->GetEntriesFast()]) TLorentzVector( met.px(),met.py(),met.pz(),met.energy()  );
@@ -47,16 +47,16 @@ int NeroMet::analyze(const edm::Event& iEvent){
         TLorentzVector metnomu(met.px(),met.py(),met.pz(),met.energy());
         TLorentzVector tkMet(0,0,0,0); 
         TLorentzVector pfmet_3p0(0,0,0,0); 
-       
+
         if ( pf == NULL ) cout<<"[NeroMet]::[analyze]::[ERROR] PF pointer is null. Run NeroPF. "<<endl; 
-        
+
         for (unsigned int i = 0, n = pf->handle->size(); i < n; ++i) {
             const pat::PackedCandidate &cand = (*pf->handle)[i];
-            
+
             // only up to eta 3
             if (std::abs(cand.pdgId()) == 13)
                 metnomu += TLorentzVector(cand.px(),cand.py(),cand.pz(),cand.energy());  
-            
+
             // only charge hadrons
             if ( cand.charge() != 0 )
                 tkMet += TLorentzVector(cand.px(),cand.py(),cand.pz(),cand.energy());
@@ -64,7 +64,7 @@ int NeroMet::analyze(const edm::Event& iEvent){
             if (std::abs(cand.eta()) < 3.0 ) 
                 pfmet_3p0 += TLorentzVector(cand.px(),cand.py(),cand.pz(),cand.energy());
         }
-       
+
         *pfMet_e3p0 = TLorentzVector( -pfmet_3p0 );
         *metNoMu = TLorentzVector(metnomu);  // no minus
         *trackMet = TLorentzVector( -tkMet );
@@ -103,27 +103,27 @@ int NeroMet::analyze(const edm::Event& iEvent){
                 cout <<"[NeroMet]::[analyze]::[WARNING] unable to translate met syst,"<< int(mysyst) <<endl;
 
             new ( (*metPuppiSyst)[ mysyst ] ) TLorentzVector( 
-                        puppi . shiftedP4( miniAODUnc).px(), 
-                        puppi . shiftedP4(miniAODUnc).py(),  
-                        puppi . shiftedP4(miniAODUnc).pz(),  
-                        puppi . shiftedP4(miniAODUnc).energy()
+                    puppi . shiftedP4( miniAODUnc).px(), 
+                    puppi . shiftedP4(miniAODUnc).py(),  
+                    puppi . shiftedP4(miniAODUnc).pz(),  
+                    puppi . shiftedP4(miniAODUnc).energy()
                     );
         }// end syst loop
 
-        }    
-        // GEN INFO
-        if ( not iEvent.isRealData () ){
-            new ( (*genP4)[genP4->GetEntriesFast()]) TLorentzVector( met.genMET()->px(),met.genMET()->py(),met.genMET()->pz(),met.genMET()->energy()  );
-        }
-
-
-        return 0;
+    }    
+    // GEN INFO
+    if ( not iEvent.isRealData () ){
+        new ( (*genP4)[genP4->GetEntriesFast()]) TLorentzVector( met.genMET()->px(),met.genMET()->py(),met.genMET()->pz(),met.genMET()->energy()  );
     }
 
-    // Local Variables:
-    // mode:c++
-    // indent-tabs-mode:nil
-    // tab-width:4
-    // c-basic-offset:4
-    // End:
-    // vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
+    return 0;
+}
+
+// Local Variables:
+// mode:c++
+// indent-tabs-mode:nil
+// tab-width:4
+// c-basic-offset:4
+// End:
+// vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
