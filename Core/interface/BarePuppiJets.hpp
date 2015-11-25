32e8cb1c798d4fabdaf34c166aae2303c686c2c5
Andrea Marini <andrea.marini@cern.ch>
2015-11-25 17:58:28 +0100

    Adding Puppi Jets to Core

---
+++ Core/interface/BarePuppiJets.hpp
@@ -0,0 +1,43 @@
+#ifndef BARE_PUPPIJETS_H
+#define BARE_PUPPIJETS_H
+
+#include "NeroProducer/Core/interface/BareCollection.hpp"
+#include "NeroProducer/Core/interface/BareP4.hpp"
+
+class BarePuppiJets : virtual public BareP4
+{
+    public:
+        enum Selection{
+            // 0--7 POG
+            JetBaseline = 1UL << 0,
+            JetLoose  = 1UL << 3, 
+            JetMedium = 1UL << 4,
+            JetTight  = 1UL << 5
+        };
+
+        BarePuppiJets();
+        ~BarePuppiJets();
+        void init() override;
+        void clear() override;
+        void defineBranches(TTree* t) override { defineBranches(t, ""); }
+        void defineBranches(TTree*, std::string prefix) override;
+
+        void setBranchAddresses(TTree* t) override { setBranchAddresses(t, ""); }
+        void setBranchAddresses(TTree*, std::string prefix) override;
+
+        inline string name() override { return "BarePuppiJets"; }
+
+        // variables --
+        vector<float> *rawPt{0};
+        vector<float> *bDiscr{0};
+        vector<unsigned> *selBits{0};
+};
+
+#endif
+// Local Variables:
+// mode:c++
+// indent-tabs-mode:nil
+// tab-width:4
+// c-basic-offset:4
+// End:
+// vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
---
+++ Core/src/BarePuppiJets.cpp
@@ -0,0 +1,53 @@
+#include "NeroProducer/Core/interface/BarePuppiJets.hpp"
+#include "NeroProducer/Core/interface/BareFunctions.hpp"
+
+BarePuppiJets::BarePuppiJets() : BareP4() {}
+
+BarePuppiJets::~BarePuppiJets(){
+    BareFunctions::Delete(rawPt);
+    BareFunctions::Delete(bDiscr);
+    BareFunctions::Delete(selBits);
+}
+
+void BarePuppiJets::init(){
+    BareP4::init();
+    BareFunctions::New(rawPt);
+    BareFunctions::New(bDiscr);
+    BareFunctions::New(selBits);
+}
+
+void BarePuppiJets::clear(){
+    BareP4::clear();
+    rawPt -> clear();
+    bDiscr -> clear();
+    selBits -> clear();
+}
+
+void BarePuppiJets::defineBranches(TTree *t, std::string prefix){
+
+    TString jetName("jet" + prefix);
+
+    BareP4::defineBranches(t, jetName.Data());
+
+    t->Branch(jetName + "RawPt","vector<float>",&rawPt);
+    t->Branch(jetName + "Bdiscr","vector<float>",&bDiscr);
+    t->Branch(jetName + "SelBits","vector<unsigned>",&selBits);
+}
+
+void BarePuppiJets::setBranchAddresses(TTree* t, std::string prefix)
+{
+    TString jetName("jet" + prefix);
+
+    BareP4::setBranchAddresses(t,jetName.Data());
+    BareFunctions::SetBranchAddress(t,jetName + "RawPt"	,&rawPt);
+    BareFunctions::SetBranchAddress(t,jetName + "Bdiscr"	,&bDiscr);
+    BareFunctions::SetBranchAddress(t,jetName + "SelBits", &selBits);
+
+}
+// Local Variables:
+// mode:c++
+// indent-tabs-mode:nil
+// tab-width:4
+// c-basic-offset:4
+// End:
+// vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
