04069f59ff51936ba70d47d9165fa3c1b7044846
Andrea Marini <andrea.marini@cern.ch>
2016-01-20 17:30:41 +0100

    max events in test

--- Nero/script/setup.sh
+++ Nero/script/setup.sh
@@ -4,6 +4,7 @@
 # [CMSSW] CMSSW_7_4_14
 # [Options] isData=False is25ns=True is50ns=False
 # [fileList] /store/mc/RunIISpring15MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/50000/E4F89698-DE6E-E511-8681-0025905A60F4.root 
+# [MaxEvents] 5000
 
 #CMSSW_7_4_5: 
 function CMSSW_7_4_5 {
