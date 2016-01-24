ab2990c255b9387190dc1a9791ae89e4d500fa19
Dmytro Kovalskyi <dmytro@cern.ch>
2016-01-24 22:09:39 +0100

    update

--- Nero/script/cutflow.py
+++ Nero/script/cutflow.py
@@ -6,6 +6,7 @@ from optparse import OptionParser
 # Get all the root classes
 from ROOT import *
 from math import *
+from cutflow_core import *
 
 gROOT.ProcessLine(".x " + os.path.dirname(os.path.realpath(__file__)) + "/functions.C");
 
@@ -46,6 +47,7 @@ else:
 
 #initialize 
 n_jet=0; n_met=0; n_njet=0; n_nlep=0; n_ntau=0; n_npho=0; n_dphi=0;n_2ndjet=0;
+n_nbjet=0
 
 # Check the number of entries in the tree
 n_entries = input_tree.GetEntriesFast()
@@ -56,62 +58,6 @@ if options.nprocs > 0 and options.nprocs < n_entries:
   n_entries = options.nprocs
 
 print 'INFO - Number of entries to be processed: ' + str(n_entries)
-
-def deltaBetaIsolation(tree,i):
-  return tree.lepChIso[i] + max(0., tree.lepPhoIso[i] + tree.lepNhIso[i] - 0.5*tree.lepPuIso[i]);
-
-def isLooseMuon(tree,i):
-  # https://twiki.cern.ch/twiki/bin/viewauth/CMS/Monojet
-  if abs(tree.lepPdgId[i]) != 13: return False
-  if abs(tree.lepP4[i].Eta()) > 2.4: return False
-  # ID
-  if (tree.lepSelBits[i] & 16) == 0: 
-    if options.debug: print "failed loose muon id"
-    return False
-  # Isolation
-  # if tree.lepIso[i]/tree.lepP4[i].Pt() > 0.20: 
-  if deltaBetaIsolation(tree,i)/tree.lepP4[i].Pt() > 0.20: 
-    if options.debug: print "failed loose muon isolation"
-    return False
-  if options.debug: print "Loose muon is found"
-  return True
-
-def isLooseElectron(tree,i):
-  # https://twiki.cern.ch/twiki/bin/viewauth/CMS/Monojet
-  if abs(tree.lepPdgId[i]) != 11: return False
-  if abs(tree.lepP4[i].Eta()) > 2.5: return False
-  # ID
-  if (tree.lepSelBits[i] & 2) == 0: 
-    if options.debug: print "failed loose electron id"
-    return False
-  # Isolation
-  # relIso = tree.lepIso[i]/tree.lepP4[i].Pt()
-  relIso = deltaBetaIsolation(tree,i)/tree.lepP4[i].Pt()
-  if abs(tree.lepP4[i].Eta()) > 1.479:
-    # endcap
-    if relIso > 0.144: 
-      if options.debug: print "failed loose electron iso endcap"
-      return False
-  else:
-    # barrel
-    if relIso > 0.126: 
-      if options.debug: print "failed loose electron iso"
-      return False
-  if options.debug: print "Loose electron is found"
-  return True
-
-def isLooseLepton(tree,i):
-  if tree.lepP4[i].Pt() < 10: return False
-  return isLooseMuon(tree,i) or isLooseElectron(tree,i)
-
-def getNLooseLeptons(tree):
-  nLeptons = tree.lepP4.GetEntriesFast()
-  if options.debug: print "nLeptons: ", nLeptons
-  nLooseLeptons = 0
-  for i in range(nLeptons):
-    if isLooseLepton(tree,i):
-      nLooseLeptons+=1
-  return nLooseLeptons
 
 # Loop over the entries
 for ientry in range(0,n_entries):
@@ -124,9 +70,18 @@ for ientry in range(0,n_entries):
 
   dphi = -10000.
 
+  #  if(input_tree.metP4[0].Energy() > 200 ):  
+  #  n_met += 1
+  # if getNLooseLeptons(input_tree)>0:
   if getNLooseLeptons(input_tree)==0:
     if options.debug: print "%d (%d) \t Event: %d" % (ientry,n_nlep,input_tree.eventNum)
     n_nlep += 1
+    if getNTaus(input_tree)==0:
+      n_ntau += 1
+      if getNLoosePhotons(input_tree)==0:
+        n_npho += 1
+        if getNBJets(input_tree)==0:
+          n_nbjet += 1
 
   # Broken
   # if(input_tree.jetP4.GetEntriesFast() > 1 and input_tree.jetP4[0].Pt() > 110 and input_tree.jetMonojetId[0] and input_tree.jetMonojetIdLoose[1]) : # and input_tree.jetPuId() > -0.62) : 
@@ -149,19 +104,19 @@ for ientry in range(0,n_entries):
   #           n_njet += 1
   #           if(input_tree.lepP4.GetEntriesFast() < 1): 
   #             n_nlep += 1
-  #             if(input_tree.tauP4.GetEntriesFast() < 1): 
-  #               n_ntau += 1
   #               if(input_tree.photonP4.GetEntriesFast() < 1):
   #                 n_npho += 1
 
 print 'INFO - Cut Flow Chart: '
 print 'INFO - FULL     '+ str(input_all.GetEntries() )
-print 'INFO - GoodVtx  '+ str(n_entries)  ,; PrintCompare(n_entries,3797)
+print 'INFO - GoodVtx  '+ str(n_entries)  ,; PrintCompare(n_entries,50492)
+print 'INFO - NLep Cut '+ str(n_nlep)     ,; PrintCompare(n_nlep,33610)
+print 'INFO - NTau Cut '+ str(n_ntau)     ,; PrintCompare(n_ntau,27598)
+print 'INFO - NPho Cut '+ str(n_npho)     ,; PrintCompare(n_npho,26806)
+print 'INFO - NBJet Cut '+ str(n_nbjet)   ,; PrintCompare(n_nbjet,5180)
+
 print 'INFO - Jet Cut  '+ str(n_jet)      ,; PrintCompare(n_jet,1995)
 print 'INFO - 2_j Cut  '+ str(n_2ndjet)   ,; PrintCompare(n_2ndjet,1990)
 print 'INFO - Phi Cut  '+ str(n_dphi)     ,; PrintCompare(n_dphi,1727)
 print 'INFO - Met Cut  '+ str(n_met)      ,; PrintCompare(n_met,996)
 print 'INFO - NJet Cut '+ str(n_njet)     ,; PrintCompare(n_njet,644)
-print 'INFO - NLep Cut '+ str(n_nlep)     ,; PrintCompare(n_nlep,644)
-print 'INFO - NTau Cut '+ str(n_ntau)     ,; PrintCompare(n_ntau,641)
-print 'INFO - NPho Cut '+ str(n_npho)     ,; PrintCompare(n_npho,637)
