186fb794e007d979f14e10ea433c373690a41d9e
Andrea Marini <andrea.marini@cern.ch>
2016-05-12 16:59:06 +0200

    updates for buildtest

--- Nero/script/setup.sh
+++ Nero/script/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Instruct builder to use a particular CMSSW release
-# [CMSSW] CMSSW_7_6_3_patch2
+# [CMSSW] CMSSW_7_6_4
 # [Options] isData=False is25ns=True is50ns=False
 # [fileList] /store/mc/RunIIFall15MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/70000/FA0A72D5-C7B8-E511-8B1D-901B0E6459E0.root 
 # [MaxEvents] 5000
