f9ec039c4bff3abe6fc3bfca4b89423667a76b57
Andrea Marini <andrea.marini@cern.ch>
2016-06-01 18:18:45 +0200

    modified setup to use Pileupjetid

--- Nero/script/setup.sh
+++ Nero/script/setup.sh
@@ -18,12 +18,19 @@ function CMSSW_7_6_4 {
 	echo /RecoMET/METAlgorithms/ >> .git/info/sparse-checkout
 	echo /RecoMET/METProducers/ >> .git/info/sparse-checkout
 	echo /EgammaAnalysis/ElectronTools/ >> .git/info/sparse-checkout
+	git cms-merge-topic jbrands:pileupJetId76X
 	git cms-merge-topic amarini:egcorrection76x
 	git cms-merge-topic cms-met:metTool76X
 	git remote add blinkseb https://github.com/blinkseb/cmssw.git
 	git fetch blinkseb
 	git cherry-pick 4cca4688ae368bbbef2102e9bdc5bb00f6df959e
 	git cms-merge-topic amarini:topic_met
+	cd RecoJets/JetProducers/data/
+	wget https://github.com/jbrands/RecoJets-JetProducers/raw/3dad903ed25d025f68be94d6f781ca957d6f86ac/pileupJetId_76x_Eta0to2p5_BDT.weights.xml.gz
+	wget https://github.com/jbrands/RecoJets-JetProducers/raw/3dad903ed25d025f68be94d6f781ca957d6f86ac/pileupJetId_76x_Eta2p5to2p75_BDT.weights.xml.gz
+	wget https://github.com/jbrands/RecoJets-JetProducers/raw/3dad903ed25d025f68be94d6f781ca957d6f86ac/pileupJetId_76x_Eta2p75to3_BDT.weights.xml.gz
+	wget https://github.com/jbrands/RecoJets-JetProducers/raw/3dad903ed25d025f68be94d6f781ca957d6f86ac/pileupJetId_76x_Eta3to5_BDT.weights.xml.gz
+	cd ../../..
 	#git clone git@github.com:zdemirag/NeroProducer.git ## TO REMOVE
 }
 
