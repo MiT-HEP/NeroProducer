0175f9e55859b9e38289a0fbd356a45188c8b51e
Andrea Marini <andrea.marini@cern.ch>
2016-06-02 18:05:09 +0200

    bug fix in pdf and scales

--- Nero/plugins/Nero.cc
+++ Nero/plugins/Nero.cc
@@ -322,6 +322,7 @@ Nero::Nero(const edm::ParameterSet& iConfig)
     info -> events_token = consumes<std::vector<long>,edm::InLumi>( edm::InputTag("InfoProducer","vecEvents") ) ;
     info -> weights_token = consumes<std::vector<float>,edm::InLumi>( edm::InputTag("InfoProducer","vecMcWeights") ) ;
     info -> putrue_token = consumes<std::vector<int>,edm::InLumi>( edm::InputTag("InfoProducer","vecPuTrueInt") ) ;
+    obj.insert(obj.begin(),info);
     lumiObj.push_back(info);
 
 
@@ -462,7 +463,10 @@ Nero::beginJob()
 
     // define branches
     for(auto o : obj)
+    {
+        if (dynamic_cast<NeroAll*> (o) !=NULL ) { continue ; }  // otherwise I will have also the branch in the main tree.
         o -> defineBranches(tree_);
+    }
 
     for(auto o : lumiObj)
         o -> defineBranches(all_);
