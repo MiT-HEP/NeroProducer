e99a432d3183e39b0274a4f63bd3860eee61608c
Andrea Marini <andrea.marini@cern.ch>
2016-09-13 17:37:56 +0200

    use signal chg hadr

--- Nero/src/NeroTaus.cpp
+++ Nero/src/NeroTaus.cpp
@@ -94,7 +94,7 @@ int NeroTaus::analyze(const edm::Event & iEvent)
         Q -> push_back( tau.charge() );
         M -> push_back( tau.mass() );
         iso -> push_back( totIso ) ; 
-        leadTrackPt -> push_back( tau.leadTrack().Pt() ) ;
+        leadTrackPt -> push_back( tau.signalChargedHadrCands()[0]->pt() );
 
         if (IsExtend() ){
             chargedIsoPtSum  -> push_back( tau.tauID("chargedIsoPtSum") );
