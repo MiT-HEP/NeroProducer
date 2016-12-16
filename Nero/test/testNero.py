e3a8429738cdc45f8cc45e85314e007dd5158332
Zeynep Demiragli <zeynep.demiragli@gmail.com>
2016-12-16 15:55:01 +0100

    adding the new JEC

--- Nero/test/testNero.py
+++ Nero/test/testNero.py
@@ -101,13 +101,13 @@ process.load("RecoEgamma/ElectronIdentification/ElectronIDValueMapProducer_cfi")
 from CondCore.DBCommon.CondDBSetup_cfi import *
 
 if options.isData:
-    connectString = cms.string('sqlite:jec/Spring16_25nsV10All_DATA.db')
-    tagName = 'Spring16_25nsV10All_DATA_AK4PFchs'
-    tagNamePuppi = 'Spring16_25nsV10All_DATA_AK4PFPuppi'
+    connectString = cms.string('sqlite:jec/Spring16_23Sep2016AllV1_DATA.db')
+    tagName = 'Spring16_23Sep2016AllV1_DATA_AK4PFchs'
+    tagNamePuppi = 'Spring16_23Sep2016AllV1_DATA_AK4PFPuppi'
 else:
-    connectString = cms.string('sqlite:jec/Spring16_25nsV10_MC.db')
-    tagName = 'Spring16_25nsV10_MC_AK4PFchs'
-    tagNamePuppi = 'Spring16_25nsV10_MC_AK4PFPuppi'
+    connectString = cms.string('sqlite:jec/Spring16_23Sep2016V1_MC.db')
+    tagName = 'Spring16_23Sep2016V1_MC_AK4PFchs'
+    tagNamePuppi = 'Spring16_23Sep2016V1_MC_AK4PFPuppi'
 
 
 process.jec = cms.ESSource("PoolDBESSource",
