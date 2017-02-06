28f7e4ce1aebbab4024de7519fce27dd82c9be84
Zeynep Demiragli <zeynep.demiragli@gmail.com>
2017-02-06 11:19:26 +0100

    updating the setup

--- Nero/script/setup.sh
+++ Nero/script/setup.sh
@@ -95,13 +95,12 @@ function CMSSW_8_0_25 {
 
 function CMSSW_8_0_26_patch1 {
         git cms-init
-        git cms-merge-topic zdemirag:resolved_met
-        git cms-merge-topic -u cms-met:fromCMSSW_8_0_20_postICHEPfilter
+        git cms-merge-topic zdemirag:conflict_resolve
         git cms-merge-topic ahinzmann:METRecipe_8020_Moriond17
         git cms-merge-topic emanueledimarco:ecal_smear_fix_80X
-        #git clone -b ICHEP2016_v2 https://github.com/ECALELFS/ScalesSmearings.git EgammaAnalysis/ElectronTools/data/ScalesSmearings
         git cms-merge-topic ikrav:egm_id_80X_v2
         git clone -b egm_id_80X_v1 https://github.com/ikrav/RecoEgamma-ElectronIdentification.git RecoEgamma/ElectronIdentification/data.new
+        git cms-addpkg RecoEgamma/ElectronIdentification
         rsync -avP RecoEgamma/ElectronIdentification/data.new/* RecoEgamma/ElectronIdentification/data/
         git cms-merge-topic ikrav:egm_id_80X_v3_photons
         git clone -b egm_id_80X_v1 https://github.com/ikrav/RecoEgamma-PhotonIdentification.git RecoEgamma/PhotonIdentification/data.new
