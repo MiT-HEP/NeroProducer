5521d7f1727de0da6dc332131b7e7d727cc4c877
Andrea Carlo Marini <andrea.marini@cern.ch>
2017-03-07 10:24:49 +0100

    script to make pu comparisons

--- Nero/script/pu-chi2.py
+++ Nero/script/pu-chi2.py
@@ -1,15 +1,16 @@
 from optparse import OptionParser
 
 parser = OptionParser(usage="usage")
-parser.add_option("-f","--file",type="string",help="Input File",default="plot/mmNpv.root")
+parser.add_option("-f","--file",type="string",help="Input File [%default]",default="plot/mmNpv.root")
 #parser.add_option("","--points",type="string",help="point numbers comma separated",default="65000,67000,68000,69000,69200,69300,69400,69500,69600,69800,70000,71000,73000")
-parser.add_option("","--points",type="string",help="point numbers comma separated",default="78000,77000,76000,75000,74000,73000,72000,71000,70800,70600,70400,70200,70000,69800,69600,69400,69200,69000,68000,67000,66000,65000,64000,63000")
-parser.add_option("-s","--scale",action='store_true',help="Scale default=%default",default=False)
+parser.add_option("","--points",type="string",help="point numbers comma separated [%default]",default="78000,77000,76000,75000,74000,73000,72000,71000,70800,70600,70400,70200,70000,69800,69600,69400,69200,69000,68000,67000,66000,65000,64000,63000")
+parser.add_option("-s","--scale",action='store_true',help="Scale [%default]",default=False)
 opts,args= parser.parse_args()
 
 import ROOT
 ROOT.gStyle.SetOptStat(0)
 ROOT.gStyle.SetOptTitle(0)
+ROOT.gStyle.SetOptFit(0111)
 
 
 fROOT = ROOT.TFile.Open(opts.file)
@@ -34,8 +35,26 @@ for n in opts.points.split(","):
 
 
 c=ROOT.TCanvas("c","c")
+c.SetBottomMargin(.15)
+c.SetLeftMargin(.15)
+c.SetTopMargin(.05)
+c.SetRightMargin(.05)
+
+th1d_chi2.Draw("AXIS")
+th1d_chi2.Draw("AXIS X+ Y+ SAME")
+
+th1d_chi2.GetXaxis().SetTitle("pu^{true}")
+th1d_chi2.GetYaxis().SetTitle("#chi^{2}")
+th1d_chi2.GetXaxis().SetTitleOffset(1.2)
+th1d_chi2.GetYaxis().SetTitleOffset(1.2)
+
 th1d_chi2.SetMarkerStyle(20)
-th1d_chi2.Draw("P")
+th1d_chi2.Draw("P SAME")
+
+f=ROOT.TF1("myf","pol2",60e3,75e3)
+th1d_chi2.Fit("myf","QN")
+th1d_chi2.Fit("myf","QM")
+#f.Draw("L SAME")
 
 raw_input("ok?")
 name ="chi2"
