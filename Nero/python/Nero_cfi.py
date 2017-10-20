73dccf84b89a27693fe933c541b577524197bcf2
Pierluigi Bortignon <bortigno@gmail.com>
2017-10-20 16:35:34 +0200

    update double muon triggers with unpreascales ones

--- Nero/python/Nero_cfi.py
+++ Nero/python/Nero_cfi.py
@@ -108,7 +108,11 @@ nero = cms.EDAnalyzer("Nero",
                                 'HLT_Mu19_TrkIsoVVL_v',
 				'HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_v',
 				'HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_v',
-				## ELECTRONS-MUONS
+				'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v2',
+                                'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v2',
+                                'HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass8_v1',
+                                'HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_Mass3p8_v1',
+                                ## ELECTRONS-MUONS
 				'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v',
 				'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v',
 				'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v',
