7788d8744eff666e6a3ae24433b7e39536d96f0f
Andrea Carlo Marini <andrea.marini@cern.ch>
2017-11-24 11:00:55 +0100

    first trigger review

--- Nero/python/Nero_cfi.py
+++ Nero/python/Nero_cfi.py
@@ -67,116 +67,123 @@ nero = cms.EDAnalyzer("Nero",
     NeroTrigger = cms.PSet ( 
         trigger = cms.InputTag("TriggerResults","","HLT"),
         objects = cms.InputTag('slimmedPatTrigger'),
-        triggerNMatch = cms.int32(12),
+        triggerNMatch = cms.int32(14),
         triggerNames = cms.vstring([
-				## LEPTON FAKES
-				'HLT_Mu8_TrkIsoVVL_v',
-				'HLT_Mu17_TrkIsoVVL_v',
-				'HLT_Ele12_CaloIdL_TrackIdL_IsoVL_PFJet30_v',
-				'HLT_Ele17_CaloIdL_TrackIdL_IsoVL_PFJet30_v',
-				'HLT_Ele23_CaloIdL_TrackIdL_IsoVL_PFJet30_v',
-				## TAU WITH MATCHING
-                'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET110',
-				'HLT_VLooseIsoPFTau140_Trk50_eta2p1_v',
-				## TAG-AND-PROBE STUDIES
-				'HLT_Ele25_eta2p1_WPTight_Gsf_v',
-				'HLT_Ele27_eta2p1_WPLoose_Gsf_v',
-				'HLT_IsoMu24_v',
-				'HLT_IsoTkMu24_v',
-	    		## ELECTRONS
-				'HLT_Ele27_WPTight_Gsf_v',
-				'HLT_Ele30_WPTight_Gsf_v',
-				'HLT_Ele27_eta2p1_WPTight_Gsf_v',
-				'HLT_Ele32_eta2p1_WPTight_Gsf_v',
-				'HLT_Ele35_WPLoose_Gsf_v',
-				'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v',
-				'HLT_DoubleEle24_22_eta2p1_WPLoose_Gsf_v',
-				## MUONS
-				'HLT_IsoMu22_v',
-	    		'HLT_IsoTkMu22_v',
-				'HLT_Mu45_eta2p1_v',
-				'HLT_Mu50_v',
-				'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v',
-				'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v',
-				'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v',
-				'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v',
-				## ELECTRONS-MUONS
-				'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v',
-				'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v',
-				'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v',
-				'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v',
-				'HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ_v',
-				'HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v',
-				'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v',
-				'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v',
-				## OTHER LEPTON TRIGGERS
-				'HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v',
-				'HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v',
-				'HLT_Mu17_v',
-                                'HLT_ECALHT800_v',
-				'HLT_Ele23_CaloIdL_TrackIdL_IsoVL_v',
-				'HLT_IsoMu20_v',
-	    			'HLT_IsoTkMu20_v',
-				'HLT_IsoMu27_v',
-				'HLT_IsoTkMu27_v',
-				'HLT_Ele22_eta2p1_WP75_Gsf_v',
-				'HLT_Ele23_WPLoose_Gsf_v',
-				'HLT_Ele27_WP85_Gsf_v',
-				'HLT_Ele27_WPLoose_Gsf_v',
-                                'HLT_Ele105_CaloIdVT_GsfTrkIdT_v',
-				## TAU
-                                'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET80', ## not always available, Tau, NOT _v because there is noise cleaned
-                                'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET90', ## not always available, Tau
-                                'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET110', ## not always available, Tau
-                                'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET120', ## unprescaled , Tau, Match
-				'HLT_LooseIsoPFTau50_Trk30_eta2p1_v', ## Prescaled, Tau, for turn on studies, Match
-				'HLT_VLooseIsoPFTau120_Trk50_eta2p1_v', ## partially prescaled, Match
-				'HLT_VLooseIsoPFTau140_Trk50_eta2p1_v', ## unprescaled, Match
-				'HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight_BTagCSV_p067_v',
-				## MET
-                                'HLT_PFMET170_NoiseCleaned',
-                                'HLT_PFMETNoMu120_NoiseCleaned_PFMHTNoMu120_IDTight',
-                                'HLT_PFMETNoMu110_NoiseCleaned_PFMHTNoMu110_IDTight',
-                                'HLT_PFMETNoMu90_NoiseCleaned_PFMHTNoMu90_IDTight',
-				'HLT_PFMET170_HBHECleaned',
-				'HLT_PFMET170_JetIdCleaned',
-				'HLT_PFMET170_NotCleaned',
-				'HLT_PFMET170_HBHE_BeamHaloCleaned',
-			        'HLT_PFMETNoMu90_PFMHTNoMu90_IDTight',
-			        'HLT_PFMETNoMu100_PFMHTNoMu100_IDTight',
-			        'HLT_PFMETNoMu110_PFMHTNoMu110_IDTight',
-			        'HLT_PFMETNoMu120_PFMHTNoMu120_IDTight',
-                                ## VBF Trigger
-                                'HLT_DiPFJet40_DEta3p5_MJJ600_PFMETNoMu140_v',
-				#### Control triggers, extra
-				'HLT_Ele15_IsoVVL_PFHT350_PFMET70',  ## Unprescaled, SingleElectron
-				'HLT_Mu15_IsoVVVL_PFHT350_PFMET70',  ## Unprescaled ,SingleMuon
-				'HLT_PFMET120_PFMHT120_IDTight',## unprescaled, MET, control on tau turn on
-				#### Photon triggers
-			        'HLT_Photon36_R9Id90_HE10_IsoM_v',
-			        'HLT_Photon50_R9Id90_HE10_IsoM_v',
-			        'HLT_Photon75_R9Id90_HE10_IsoM_v',
-			        'HLT_Photon90_R9Id90_HE10_IsoM_v',
-			        'HLT_Photon120_R9Id90_HE10_IsoM_v',
-			        'HLT_Photon165_R9Id90_HE10_IsoM_v',
-				'HLT_Photon175',
-				'HLT_Photon165_HE10',
-                ### Hadronic Triggers
-                'HLT_PFHT_475_v',
-                'HLT_PFHT_800_v',
-                'HLT_AK8PFJet360_TrimMass30_v',
-                'HLT_AK8PFHT650_TrimR0p1PT0p3Mass50_v',
-				### DoubleMu / DoubleE cross check and sync with Z
-				'HLT_Ele17_CaloIdL_TrackIdL_IsoVL_v',
-                                'HLT_CaloMET200_NoiseCleaned',
-                                'Flag_eeBadScFilter',
-                                'Flag_HBHENoiseFilter',
-                                'Flag_HBHENoiseIsoFilter',
-				### L1  ## these needs to be implemented in the code as well
-				'L1_ET80',
-				'L1_ET90',
-				'L1_ET100',
-				]),
+            ## LEPTON FAKES
+            'HLT_Mu8_TrkIsoVVL_v',
+            'HLT_Mu17_TrkIsoVVL_v',
+            'HLT_Ele12_CaloIdL_TrackIdL_IsoVL_PFJet30_v',
+            'HLT_Ele17_CaloIdL_TrackIdL_IsoVL_PFJet30_v',
+            'HLT_Ele23_CaloIdL_TrackIdL_IsoVL_PFJet30_v',
+            ## TAU WITH MATCHING
+            'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET110',
+            'HLT_VLooseIsoPFTau140_Trk50_eta2p1_v',
+            ## TAG-AND-PROBE STUDIES
+            'HLT_Ele25_eta2p1_WPTight_Gsf_v',
+            'HLT_Ele27_eta2p1_WPLoose_Gsf_v',
+            'HLT_IsoMu24_v',
+            'HLT_IsoTkMu24_v',
+            'HLT_IsoMu27_v',
+            'HLT_IsoTkMu27_v',
+            ### NO  TRIGGER MATCHING ###
+            ## ELECTRONS
+            'HLT_Ele27_WPTight_Gsf_v',
+            'HLT_Ele30_WPTight_Gsf_v',
+            'HLT_Ele27_eta2p1_WPTight_Gsf_v',
+            'HLT_Ele32_eta2p1_WPTight_Gsf_v',
+            'HLT_Ele35_WPLoose_Gsf_v',
+            'HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v',
+            'HLT_DoubleEle24_22_eta2p1_WPLoose_Gsf_v',
+            ## MUONS
+            'HLT_IsoMu20_v',
+            'HLT_IsoMu22_v',
+            'HLT_IsoMu30_v',
+            'HLT_IsoTkMu20_v',
+            'HLT_IsoTkMu22_v',
+            'HLT_Mu19_TrkIsoVVL_v',
+            'HLT_Mu45_eta2p1_v',
+            'HLT_Mu50_v',
+            ## dimuon
+            #
+            'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_v',
+            'HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_v',
+            'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_DZ_v',
+            'HLT_Mu17_TrkIsoVVL_TkMu8_TrkIsoVVL_v',
+            'HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_DZ_v',
+            'HLT_Mu19_TrkIsoVVL_Mu9_TrkIsoVVL_v',
+            ## ELECTRONS-MUONS
+            'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v',
+            'HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v',
+            'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v',
+            'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v',
+            'HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_DZ_v',
+            'HLT_Mu23_TrkIsoVVL_Ele8_CaloIdL_TrackIdL_IsoVL_v',
+            'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v',
+            'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v',
+            'HLT_DoubleIsoMu20_eta2p1_v',
+            'HLT_DoubleIsoMu24_eta2p1_v',
+            ## OTHER LEPTON TRIGGERS
+            'HLT_Mu17_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v',
+            'HLT_Mu8_TrkIsoVVL_Ele17_CaloIdL_TrackIdL_IsoVL_v',
+            'HLT_Mu17_v',
+            'HLT_ECALHT800_v',
+            'HLT_Ele23_CaloIdL_TrackIdL_IsoVL_v',
+            'HLT_Ele22_eta2p1_WP75_Gsf_v',
+            'HLT_Ele23_WPLoose_Gsf_v',
+            'HLT_Ele27_WP85_Gsf_v',
+            'HLT_Ele27_WPLoose_Gsf_v',
+            'HLT_Ele105_CaloIdVT_GsfTrkIdT_v',
+            ## TAU
+            'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET80', ## not always available, Tau, NOT _v because there is noise cleaned
+            'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET90', ## not always available, Tau
+            'HLT_LooseIsoPFTau50_Trk30_eta2p1_MET120', ## unprescaled , Tau, Match
+            'HLT_LooseIsoPFTau50_Trk30_eta2p1_v', ## Prescaled, Tau, for turn on studies, Match
+            'HLT_VLooseIsoPFTau120_Trk50_eta2p1_v', ## partially prescaled, Match
+            #'HLT_CaloMHTNoPU90_PFMET90_PFMHT90_IDTight_BTagCSV_p067_v',
+            ## MET
+            'HLT_PFMET170_NoiseCleaned',
+            'HLT_PFMETNoMu120_NoiseCleaned_PFMHTNoMu120_IDTight',
+            'HLT_PFMETNoMu110_NoiseCleaned_PFMHTNoMu110_IDTight',
+            'HLT_PFMETNoMu90_NoiseCleaned_PFMHTNoMu90_IDTight',
+            'HLT_PFMET170_HBHECleaned',
+            'HLT_PFMET170_JetIdCleaned',
+            'HLT_PFMET170_NotCleaned',
+            'HLT_PFMET170_HBHE_BeamHaloCleaned',
+            'HLT_PFMETNoMu90_PFMHTNoMu90_IDTight',
+            'HLT_PFMETNoMu100_PFMHTNoMu100_IDTight',
+            'HLT_PFMETNoMu110_PFMHTNoMu110_IDTight',
+            'HLT_PFMETNoMu120_PFMHTNoMu120_IDTight',
+            ## VBF Trigger
+            'HLT_DiPFJet40_DEta3p5_MJJ600_PFMETNoMu140_v',
+            #### Control triggers, extra
+            'HLT_Ele15_IsoVVL_PFHT350_PFMET70',  ## Unprescaled, SingleElectron
+            'HLT_Mu15_IsoVVVL_PFHT350_PFMET70',  ## Unprescaled ,SingleMuon
+            'HLT_PFMET120_PFMHT120_IDTight',## unprescaled, MET, control on tau turn on
+            #### Photon triggers
+            #'HLT_Photon36_R9Id90_HE10_IsoM_v',
+            #'HLT_Photon50_R9Id90_HE10_IsoM_v',
+            #'HLT_Photon75_R9Id90_HE10_IsoM_v',
+            #'HLT_Photon90_R9Id90_HE10_IsoM_v',
+            #'HLT_Photon120_R9Id90_HE10_IsoM_v',
+            #'HLT_Photon165_R9Id90_HE10_IsoM_v',
+            'HLT_Photon175',
+            'HLT_Photon165_HE10',
+            ### Hadronic Triggers
+            'HLT_PFHT_475_v',
+            'HLT_PFHT_800_v',
+            'HLT_AK8PFJet360_TrimMass30_v',
+            'HLT_AK8PFHT650_TrimR0p1PT0p3Mass50_v',
+            ### DoubleMu / DoubleE cross check and sync with Z
+            'HLT_Ele17_CaloIdL_TrackIdL_IsoVL_v',
+            'HLT_CaloMET200_NoiseCleaned',
+            'Flag_eeBadScFilter',
+            'Flag_HBHENoiseFilter',
+            'Flag_HBHENoiseIsoFilter',
+            ### L1  ## these needs to be implemented in the code as well
+            'L1_ET80',
+            'L1_ET90',
+            'L1_ET100',
+            ]),
 
         ),
 
