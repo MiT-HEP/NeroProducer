62152ae16cf82cfef26f07ba4c42027f42c2be12
Andrea Carlo Marini <andrea.marini@cern.ch>
2018-01-24 13:38:49 +0100

    move to dasgoclient

--- Nero/script/datasetinfo.sh
+++ Nero/script/datasetinfo.sh
@@ -3,8 +3,10 @@
 DATASET=$1
 echo "DATASET=$DATASET"
 
-RUNRANGE=$(das_client.py --query "run dataset=$DATASET" --limit=200 | sort -n |grep -v "Showing" | grep -v '^$' | sed -ne '1p;$p' | tr '\n' ',')
-EVENTS=$(das_client.py --query "summary dataset=$DATASET" | grep 'nevents\|nlumis' | cut -d':' -f 2 | tr '\n' ',')
+#RUNRANGE=$(das_client.py --query "run dataset=$DATASET" --limit=200 | sort -n |grep -v "Showing" | grep -v '^$' | sed -ne '1p;$p' | tr '\n' ',')
+#EVENTS=$(das_client.py --query "summary dataset=$DATASET" | grep 'nevents\|nlumis' | cut -d':' -f 2 | tr '\n' ',')
+RUNRANGE=$(dasgoclient --query "run dataset=$DATASET" --limit=200 | sort -n |grep -v "Showing" | grep -v '^$' | sed -ne '1p;$p' | tr '\n' ',')
+EVENTS=$(dasgoclient --query "summary dataset=$DATASET" | grep 'nevents\|nlumis' | cut -d':' -f 2 | tr '\n' ',')
 
 echo "#runrange -- events, lumi"
 echo "$RUNRANGE  - $EVENTS"
