f0f2e9d6d6d843fda820812aa494921b2417893b
Andrea Carlo Marini <andrea.marini@cern.ch>
2019-03-07 11:19:57 +0100

    JEC 102X V8

--- Nero/script/setup.sh
+++ Nero/script/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Instruct builder to use a particular CMSSW release
-# [CMSSW] CMSSW_9_4_9
+# [CMSSW] CMSSW_10_2_9
 # [Options] isData=False
-# [fileList] /store/user/amarini/Sync/5AC9148F-9842-E811-892B-3417EBE535DA.root
+# [fileList] /store/mc/RunIIAutumn18MiniAOD/DYJetsToLL_M-105To160_TuneCP5_PSweights_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/102X_upgrade2018_realistic_v15-v1/100000/48A13F85-97BC-8348-A555-9AC79DF6628B.root
 # [MaxEvents] 5000
 # [Tag] CMSSW_92X
 
