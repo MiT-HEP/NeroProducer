67d49f116432bf357618a4f9d20f724b1bb94e8f
Andrea Carlo Marini <andrea.marini@cern.ch>
2019-12-21 19:58:52 +0100

    setup for 10 2 15

--- Nero/script/setup.sh
+++ Nero/script/setup.sh
@@ -107,6 +107,24 @@ index 33994ce..2b8dd24 100644
 EOF
 }
 
+function CMSSW_10_2_15 {
+        git cms-init
+        git cms-merge-topic cms-egamma:EgammaPostRecoTools
+        git cms-merge-topic cms-egamma:EgammaID_1023
+        git cms-merge-topic cms-egamma:slava77-btvDictFix_10210 #fixes the Run2018D dictionary issue, see https://github.com/cms-sw/cmssw/issues/26182, may not be necessary for later releases, try it first and see if it works
+        #now to add the scale and smearing for 2018 (eventually this will not be necessary in later releases but is harmless to do regardless)
+        git cms-addpkg EgammaAnalysis/ElectronTools
+        rm EgammaAnalysis/ElectronTools/data -rf
+        git clone git@github.com:cms-data/EgammaAnalysis-ElectronTools.git EgammaAnalysis/ElectronTools/data
+        git cms-merge-topic cms-met:METFixEE2017_949_v2_backport_to_102X
+        ## additional variables for qg: (axis1,cmult,nmult). Optional 
+        git cms-merge-topic amarini:topic_qgmorvar_102X 
+        #git cms-merge-topic amarini:fix_102x_met_ptdepjer
+        git cms-merge-topic amarini:fix_102_15_met_ptdepjer
+        git clone ssh://git@gitlab.cern.ch:7999/uhh-cmssw/fsr-photon-recovery.git FSRPhotonRecovery
+
+}
+
 
 # expected default
 [ "X$1" == "X" ] && $1=$CMSSW_VERSION
